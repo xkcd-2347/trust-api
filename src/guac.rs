afc824afe9581cee27d3670092c1729858dc632b
Ulf Lilleengen <lulf@redhat.com>
2023-04-21 19:43:18 +0200

    add cvss3 and severity

--- src/guac.rs
+++ src/guac.rs
@@ -2,6 +2,7 @@ use crate::package::Package;
 use crate::package::PackageDependencies;
 use crate::package::PackageRef;
 use crate::package::VulnerabilityRef;
+use crate::vulnerability::Cvss3;
 use crate::vulnerability::Vulnerability;
 use anyhow::anyhow;
 use guac_rs::client::GuacClient;
@@ -96,6 +97,8 @@ impl Guac {
         );
         let response = reqwest::get(hydra).await;
         let mut summary = "Unavailable".to_string();
+        let mut severity = None;
+        let mut cvss3 = None;
         if let Ok(response) = response {
             if response.status() == StatusCode::OK {
                 if let Ok(data) = response.json::<serde_json::Value>().await {
@@ -104,6 +107,23 @@ impl Guac {
                             summary = details.to_string();
                         }
                     }
+                    if let Some(Some(data)) = data.get("cvss3").map(|s| s.as_object()) {
+                        match (
+                            data.get("cvss3_base_score").map(|s| s.as_str()),
+                            data.get("status").map(|s| s.as_str()),
+                        ) {
+                            (Some(Some(score)), Some(Some(status))) => {
+                                cvss3.replace(Cvss3 {
+                                    score: score.to_string(),
+                                    status: status.to_string(),
+                                });
+                            }
+                            _ => {}
+                        }
+                    }
+                    if let Some(Some(data)) = data.get("thread_severity").map(|s| s.as_str()) {
+                        severity.replace(data.to_string());
+                    }
                 }
             }
         }
@@ -111,6 +131,8 @@ impl Guac {
         Ok(Vulnerability {
             cve: cve_id.to_string(),
             summary,
+            severity,
+            cvss3,
             // TODO: Avoid hardcoding url, get from guac
             advisory: format!(
                 "https://access.redhat.com/security/cve/{}",
